9b962be4ebfd6dba1a0370ad97aecc11d534a494
Jay Smith <jayvsmith@gmail.com>
2017-11-24 00:06:50 -0600

    Shamefull TODOs

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -1,4 +1,4 @@
-// Package for resolving the IP addresses of hosts in the DownloadRequest struct.
+// Package for resolving the IP addresses of hosts in the Request interface.
 package resolver
 
 import (
@@ -7,12 +7,14 @@ import (
 	"sync"
 )
 
+//TODO: NO. Update to no longer use a worker thread model.
 const resolverThreads = 5
 
 // The following values can be stubbed out during testing
 var dnsResolver = net.LookupIP
 
-type ResolverError struct {
+//TODO: Update error handling to be consistent with downloader 
+type Error struct {
 	HostName    string
 	Description string
 }
@@ -23,16 +25,16 @@ type Request interface {
 	SetAddress(net.IP)
 }
 
-func (e *ResolverError) Error() string {
+func (e *Error) Error() string {
 	return fmt.Sprintf("%s: %s", e.HostName, e.Description)
 }
 
 // Receives a channel of DownloadRequest pointers and resolves their IP address.
 // Returns a channel of DownloadRequest pointers with their IP address populated.
-// Also returns a channel of ResolverErrors
-func Resolver(requests <-chan Request) (<-chan Request, <-chan *ResolverError) {
+// Also returns a channel of Errors
+func Resolver(requests <-chan Request) (<-chan Request, <-chan *Error) {
 	resolved := make(chan Request)
-	errors := make(chan *ResolverError)
+	errors := make(chan *Error)
 
 	//TODO: proper cache that expires entries rather than eat all memory
 	cache := make(map[string]net.IP)
@@ -51,7 +53,7 @@ func Resolver(requests <-chan Request) (<-chan Request, <-chan *ResolverError) {
 	return resolved, errors
 }
 
-func resolveWorker(request Request, results chan<- Request, errors chan<- *ResolverError, cache map[string]net.IP, lock *sync.RWMutex, wg *sync.WaitGroup) {
+func resolveWorker(request Request, results chan<- Request, errors chan<- *Error, cache map[string]net.IP, lock *sync.RWMutex, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	lock.RLock()
@@ -61,7 +63,7 @@ func resolveWorker(request Request, results chan<- Request, errors chan<- *Resol
 		responseIps, err := dnsResolver(request.HostName())
 
 		if err != nil {
-			errors <- &ResolverError{HostName: request.HostName(), Description: err.Error()}
+			errors <- &Error{HostName: request.HostName(), Description: err.Error()}
 		} else {
 			lock.Lock()
 			cache[request.HostName()] = responseIps[0]
