36709a15755a6b17f52ee713a4721df8b255f6c0
Jay Smith <jayvsmith@gmail.com>
2017-11-27 04:21:21 -0600

    Host-based blacklisting implemented in the filter. Now with 100% less mutex.

--- filter/filter.go
+++ filter/filter.go
@@ -4,61 +4,157 @@
 package filter
 
 import (
+	"net/http"
 	"net/url"
 	"sync"
 )
 
-// we might want something like a red-black tree for performance in the future
-// but for now, lets use the built in maps
-type filter struct {
-	urls    map[string]bool
-	urlLock sync.RWMutex
+// Urls will be partitioned by host, removing the need for locks around the host state.
+// One goroutine per host, not per url.
+// HACK: maps may not be the most efficient data structure for urlsSeen,
+//       but we'll try it first before decided if something like a red-black tree
+//       is required.
+type host struct {
+	blacklist      chan bool
+	failedAttempts int
+	urlsSeen       map[string]bool
+	requests       chan *url.URL
+	httpResp       chan *http.Response
+	response       chan<- *url.URL
 }
 
-func (f *filter) isValid(u *url.URL) bool {
-
-	f.urlLock.RLock()
-	urlSeenBefore := f.urls[u.String()]
-	f.urlLock.RUnlock()
-
-	if urlSeenBefore {
+func (h *host) isValid(u *url.URL) bool {
+	isRepeat := h.urlsSeen[u.String()]
+	if isRepeat {
 		return false
 	}
 
-	f.urlLock.Lock()
-	//Always check again after locking
-	urlSeenBefore = f.urls[u.String()]
-	if !urlSeenBefore {
-		f.urls[u.String()] = true
-	}
-	f.urlLock.Unlock()
-	return !urlSeenBefore
+	h.urlsSeen[u.String()] = true
+	return true
 }
 
-// Create takes a channel of urls, and returns a
-// channel of only urls that are safe to download
-func Create(requests <-chan *url.URL) <-chan *url.URL {
-	responses := make(chan *url.URL)
+func (h *host) run(wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer close(h.blacklist)
+	defer close(h.httpResp)
 
-	f := &filter{
-		urls:    make(map[string]bool),
-		urlLock: sync.RWMutex{},
-	}
-
-	var wg sync.WaitGroup
-	go func() {
-		defer close(responses)
-		for r := range requests {
-			wg.Add(1)
-			go func(r *url.URL) {
-				defer wg.Done()
-				if f.isValid(r) {
-					responses <- r
-				}
-			}(r)
-
+	blacklisted := false
+	for {
+		select {
+		case u, ok := <-h.requests:
+			if !ok {
+				return
+			}
+			if !blacklisted && h.isValid(u) {
+				h.response <- u
+			}
+		case blacklisted = <-h.blacklist:
 		}
-		wg.Wait()
-	}()
-	return responses
+	}
+}
+
+type Filter interface {
+	Test(*url.URL)
+	Results() <-chan *url.URL
+	Blacklist(string)
+	ProcessResponse(*http.Response)
+	Close()
+}
+
+type filter struct {
+	requests      chan *url.URL
+	results       chan *url.URL
+	hosts         map[string]*host
+	blacklist     chan string
+	responses     chan *http.Response
+	hostWaitGroup sync.WaitGroup
+}
+
+func Create() Filter {
+	f := &filter{
+		requests:      make(chan *url.URL),
+		results:       make(chan *url.URL),
+		hosts:         make(map[string]*host),
+		blacklist:     make(chan string),
+		responses:     make(chan *http.Response),
+		hostWaitGroup: sync.WaitGroup{},
+	}
+	go f.run()
+	return f
+}
+
+func (f *filter) Test(u *url.URL) {
+	f.requests <- u
+}
+
+func (f *filter) Results() <-chan *url.URL {
+	return f.results
+}
+
+func (f *filter) Blacklist(host string) {
+	f.blacklist <- host
+}
+
+func (f *filter) ProcessResponse(r *http.Response) {
+	f.responses <- r
+}
+
+func (f *filter) Close() {
+	close(f.requests)
+}
+
+func (f *filter) getHost(hst string) *host {
+	h, hostExists := f.hosts[hst]
+	if !hostExists {
+		// Using small buffered channels to communicate with the hosts
+		// so delays in processing one host's request will not block the
+		// main filter when a subsequent request is sent to the same host
+		h = &host{
+			blacklist: make(chan bool),
+			requests:  make(chan *url.URL, 5),
+			httpResp:  make(chan *http.Response, 2),
+			response:  f.results,
+			urlsSeen:  make(map[string]bool),
+		}
+		f.hosts[hst] = h
+		f.hostWaitGroup.Add(1)
+		go h.run(&f.hostWaitGroup)
+	}
+	return h
+}
+
+func (f *filter) closeHosts() {
+	// I don't use a shared `done` channel to close these all at once,
+	// because the hosts have buffered channels that they must finish
+	// processing without interruption before shutting down. Closing
+	// the channels will allow them to read the channel buffer until
+	// it's empty before shutting down.
+	for _, host := range f.hosts {
+		close(host.requests)
+	}
+	f.hostWaitGroup.Wait()
+}
+
+func (f *filter) run() {
+	defer close(f.responses)
+	defer close(f.blacklist)
+	defer close(f.results)
+
+	for {
+		select {
+		case u, ok := <-f.requests:
+			if !ok {
+				f.closeHosts()
+				return
+			}
+			host := f.getHost(u.Host)
+			host.requests <- u
+		case hostKey := <-f.blacklist:
+			host := f.getHost(hostKey)
+			host.blacklist <- true
+		case httpResp := <-f.responses:
+			host := f.getHost(httpResp.Request.URL.Host)
+			host.httpResp <- httpResp
+		}
+	}
 }
--- filter/filter_test.go
+++ filter/filter_test.go
@@ -1,27 +1,139 @@
 package filter
 
 import (
+	"net/http"
 	"net/url"
 	"testing"
+	"time"
 )
 
-func TestFilterBlocksDuplicates(t *testing.T) {
-	requests := make(chan *url.URL)
-	responses := Create(requests)
+func timer(signal chan bool, t time.Duration) {
+	defer close(signal)
+	time.Sleep(t)
+}
+
+func TestIncrementalBackoffOnError(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping long-running incremental back-off test")
+	}
+	filter := Create()
+	defer filter.Close()
+
+	var timeSignal chan bool
+	responseUrl, _ := url.Parse("http://monkey.com/test")
+	response := &http.Response{
+		StatusCode: 500,
+		Request:    &http.Request{URL: responseUrl},
+	}
+
+	var request *url.URL
+	// the first request should pass the filter in under 10ms
+	timeSignal = make(chan bool)
+	request, _ = url.Parse("http://monkey.com/test")
+
+	go filter.Test(request)
+	go timer(timeSignal, 5*time.Millisecond)
+
+	select {
+	case <-filter.Results():
+		// Good, response returned first
+	case <-timeSignal:
+		t.Errorf("Initial response was too slow")
+		return
+	}
+
+	filter.ProcessResponse(response)
+	// Now that the first 500 has returned, the next request should be delayed
+	// by over 90ms
+	timeSignal = make(chan bool)
+	request, _ = url.Parse("http://monkey.com/test1")
+	go filter.Test(request)
+	go timer(timeSignal, 90*time.Millisecond)
+	select {
+	case <-filter.Results():
+		t.Errorf("First delay was too short")
+		return
+	case <-timeSignal:
+		// Good, at least 90ms should pass
+	}
+	<-filter.Results()
+
+	filter.ProcessResponse(response)
+	filter.ProcessResponse(response)
+	// After two more 500 errors, the next request should be
+	// delayed by at least 300ms
+	timeSignal = make(chan bool)
+	request, _ = url.Parse("http://monkey.com/test2")
+	go filter.Test(request)
+	go timer(timeSignal, 300*time.Millisecond)
+	select {
+	case <-filter.Results():
+		t.Errorf("Second delay was too short")
+		return
+	case <-timeSignal:
+		// Good, at least 90ms should pass
+	}
+	<-filter.Results()
+
+	// Message to a different domain should pass instantly
+	timeSignal = make(chan bool)
+	request, _ = url.Parse("http://pie.com/test")
+	go filter.Test(request)
+	go timer(timeSignal, 5*time.Millisecond)
+	select {
+	case <-filter.Results():
+		// Good, response returned first
+	case <-timeSignal:
+		t.Errorf("Un-delayed domain was too slow")
+		return
+	}
+}
+
+func TestFiltersBlacklistsDomain(t *testing.T) {
+	filter := Create()
+
+	filter.Blacklist("monkey.com")
 
 	go func() {
-		defer close(requests)
+		defer filter.Close()
+		url1, _ := url.Parse("http://monkey.com/test1.html")
+		url2, _ := url.Parse("http://pie.com/test1.html")
+		filter.Test(url1)
+		filter.Test(url2)
+	}()
+
+	//First response should pass through as normal
+	pieReturned := false
+	for response := range filter.Results() {
+		if response.Host == "monkey.com" {
+			t.Errorf("Blacklisted domain not filtered")
+		} else if response.Host == "pie.com" {
+			pieReturned = true
+		}
+	}
+
+	if !pieReturned {
+		t.Errorf("non-blacklisted domain incorrectly filtered")
+	}
+}
+
+func TestFilterBlocksDuplicates(t *testing.T) {
+	filter := Create()
+
+	go func() {
+		defer filter.Close()
 		url1, _ := url.Parse("http://monkey.com/test1.html")
 		url2, _ := url.Parse("http://monkey.com/test1.html")
-		requests <- url1
-		requests <- url2
+		filter.Test(url1)
+		filter.Test(url2)
 	}()
-	//First response should pass through as normal
-	<-responses
 
-	//Channel will close and other response should be nil,
-	resp, ok := <-responses
-	if ok {
-		t.Errorf("Duplicate response returned: %s", resp.String())
+	//Only one result should return
+	responseCount := 0
+	for range filter.Results() {
+		responseCount++
+	}
+	if responseCount > 1 {
+		t.Errorf("Duplicate response returned")
 	}
 }
